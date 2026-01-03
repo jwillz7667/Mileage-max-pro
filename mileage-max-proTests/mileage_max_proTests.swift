01ba816227522b053a82fc8cc877e8ffa1f58e57
jwillz7667 <jwillz7667@gmail.com>
2026-01-02 21:35:58 -0600

    Initial Commit

---
+++ mileage-max-proTests/mileage_max_proTests.swift
@@ -0,0 +1,17 @@
+//
+//  mileage_max_proTests.swift
+//  mileage-max-proTests
+//
+//  Created by Justin Williams on 1/2/26.
+//
+
+import Testing
+@testable import mileage_max_pro
+
+struct mileage_max_proTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
